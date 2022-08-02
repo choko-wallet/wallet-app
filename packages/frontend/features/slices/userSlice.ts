16c3b587749be8cb373bd1abfe3bb10ca4eb1753
rocalex <rocmaildev@gmail.com>
2022-08-02 11:40:09 +0800

    updated the redux

--- packages/frontend/features/slices/userSlice.ts
+++ packages/frontend/features/slices/userSlice.ts
@@ -1,16 +1,32 @@
 // Copyright 2021-2022 @choko-wallet/frontend authors & contributors
 // SPDX-License-Identifier: Apache-2.0
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { UserAccount, LockedPrivateKey } from '@choko-wallet/core';
 
-import { mnemonicToMiniSecret } from '@polkadot/util-crypto';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+const initial = createAsyncThunk(
+  'users/init',
+  async (userAccount: UserAccount) => {
+    await userAccount.init();
+  }
+)
 
-import { UserAccount } from '@choko-wallet/core';
+const signMessage = createAsyncThunk(
+  'users/signMessage',
+  async (payload: SignMessagePayload) => {
+    const {userAccount, message} = payload;
+    const response: Uint8Array = await userAccount.signMessage(message);
+    return response;
+  }
+)
 
+interface SignMessagePayload {
+  userAccount: UserAccount;
+  message: Uint8Array;
+}
 interface UserSliceItem {
   userAccount: UserAccount;
-  m12Seeds: string;
-  privateKey: Uint8Array;
-  serializedUserAccount: Uint8Array
+  lockedPrivateKey: LockedPrivateKey;
+  signMessage: Uint8Array;
 }
 
 const initialState: UserSliceItem = {
@@ -19,37 +35,51 @@ const initialState: UserSliceItem = {
     keyType: 'sr25519',
     localKeyEncryptionStrategy: 0
   }),
-  m12Seeds: undefined,
-  privateKey: undefined,
-  serializedUserAccount: undefined
+  lockedPrivateKey: null,
+  signMessage: null
 };
 
 export const userSlice = createSlice({
   initialState,
   name: 'user',
-  reducers: {
 
+  reducers: {
     // save userAccount
     seedToUserAccount: (state, action: PayloadAction<string>) => {
-      state.m12Seeds = action.payload;
-      state.privateKey = mnemonicToMiniSecret(action.payload);
-
       const userAccount = UserAccount.seedToUserAccount(action.payload, {
         hasEncryptedPrivateKeyExported: false,
         keyType: 'sr25519', // sr25519
         localKeyEncryptionStrategy: 0 // password-v0
       });
-
       state.userAccount = userAccount;
     },
 
     // lock userAccount
     lockUserAccount: (state, action: PayloadAction<Uint8Array>) => {
+      const passwordHash = action.payload;
       const userAccount = state.userAccount;
-      const lockedPrivateKey = userAccount.lockUserAccount(action.payload);
+      state.lockedPrivateKey = userAccount.lockUserAccount(passwordHash);
+    },
 
-      state.serializedUserAccount = lockedPrivateKey.serialize();
+    // unlock userAccount
+    unlockUserAccount: (state, action: PayloadAction<Uint8Array>) => {
+      const passwordHash = action.payload;
+      const userAccount = UserAccount.unlockUserAccount(state.lockedPrivateKey, passwordHash);
+      state.userAccount = userAccount;
     }
+  },
+
+  extraReducers: (builder) => {
+    // init
+    builder.addCase(initial.fulfilled, (state, action) => {
+      console.log("initialized");
+    }),
+    
+    // signMessage
+    builder.addCase(signMessage.fulfilled, (state, action) => {
+      state.signMessage = action.payload;
+      console.log("signed message");
+    })
   }
 });
 
