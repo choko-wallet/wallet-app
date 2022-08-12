5f6fa1c981438a3a24bae29d6fbb4627133247d2
Song Zhou <zs@ziorix.com>
2022-08-12 21:03:41 +0800

    add import seed page

---
+++ packages/frontend/pages/import.tsx
@@ -0,0 +1,174 @@
+// Copyright 2021-2022 @choko-wallet/frontend authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { CheckIcon, XIcon, ArrowSmLeftIcon, ArrowSmRightIcon} from '@heroicons/react/outline';
+import { mnemonicValidate } from '@polkadot/util-crypto';
+
+function ImportWallet(): JSX.Element {
+
+  const router = useRouter();
+  const [mounted, setMounted] = useState<boolean>(false);
+// const { theme } = useTheme();
+
+  const [step, setStep] = useState<number>(1);
+  
+  const [seeds, setSeeds] = useState<Array<string>>(Array(12).fill(''));
+  
+  const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
+  
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const handlePaste = (e: ClipboardEvent) => {
+    // @ts-ignore
+    const text = e.clipboardData.getData('text') || window.clipboardData.getData('text');
+    if (text) {
+      setSeeds(text.split(' '));
+    }
+  }
+
+  // @ts-ignore
+  window.addEventListener('paste', handlePaste);
+
+
+  return (
+    <main className='grid grid-cols-12 gap-4 h-screen content-center bg-gray-400'>
+
+      <ul className="steps col-span-6 col-start-4">
+        <li className={`step ${ step > 0 ? 'step-neutral': ''}`}>
+          Type In Your Mnemonic
+        </li>
+        <li className={`step ${ step > 1 ? 'step-neutral': ''}`}>
+          Set a Password
+        </li>
+      </ul>
+
+      {/* TODO: these are used to create more space between the steps and main content. 
+      We should have a better way to deal with it  */}
+      <div className='col-span-12'></div>
+      <div className='col-span-12'></div>
+      <div className='col-span-12'></div>
+      <div className='col-span-12'></div>
+      <div className='col-span-12'></div>
+      <div className='col-span-12'></div> 
+
+      {step === 1 &&  
+      
+      <div className='grid grid-cols-12 col-span-4 col-start-5 gap-y-5'>
+        <div className=' col-span-12 shadow-xl rounded-lg'>
+          <div className='card p-10 ' style={{background: 'white'}}>
+            <h2 className='card-title'>
+              Import Mnemonic 
+            </h2>
+            <h3>Import a 12 words mnemonic.</h3>
+
+            <div className='grid grid-cols-8 gap-5 m-10 select-none'>
+              {seeds.map((i, index) => 
+                <div key={index} className='col-span-2 p-2 text-center rounded-lg'>
+                  <input type="text" 
+                    placeholder={`Word #${index + 1}`}
+                    onChange={(e) => setSeeds(
+                      seeds.map((seed, i) => i === index ? e.target.value : seed)
+                    )}
+                    value={seeds[index]}  
+                    className="input input-bordered w-full max-w-xs" 
+                  /> 
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        
+        <div className='col-span-12'></div>  
+
+        <div className='col-span-3 col-start-5'>
+          <button className='btn btn-error btn-circle btn-lg'
+            onClick={() => setStep(1)} >
+            <XIcon className='h-8 duration-300 hover:scale-125 transtion east-out' />
+          </button>
+        </div>
+        <div className='col-span-3'>
+        <button className={`btn btn-accent btn-circle btn-lg ${( mnemonicValidate( seeds.join(' ') )) ? '' : 'btn-disabled'}`} 
+              onClick={() => {
+                setStep(2);
+              }} >
+              <ArrowSmRightIcon className='h-8 duration-300 hover:scale-125 transtion east-out' />
+            </button>
+        </div>
+
+        
+      </div>
+      }
+
+      {step === 2 &&  
+        <div className='grid grid-cols-12 col-span-4 col-start-5 gap-y-5'>
+          <div className='col-span-12 shadow-xl rounded-lg'>
+            <div className='card p-10 ' style={{background: 'white'}}>
+              <h2 className='card-title'>
+                Set a Password
+              </h2>
+              <h3>Set a local password for you walelt. </h3>
+
+              <div className='grid grid-cols-8 gap-5 m-4 mt-8'>
+                {/* TODO: Should not use this pt-2, but some vertical-align instead */}
+                <h2 className='col-span-2 align-center pt-2'>
+                  <span className=''>Set a good password</span>
+                </h2>
+                <div className='col-span-4'>
+                  <input type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password} 
+
+                    placeholder="Set a Password" 
+                    className="input input-bordered w-full max-w-xs" 
+                  />
+                </div>
+              </div>
+              <div className='grid grid-cols-8 gap-5 m-4'>
+                {/* TODO: Should not use this pt-2, but some vertical-align instead */}
+                <h2 className='col-span-2 align-center pt-2'>
+                  <span className=''>Repeat Password</span>
+                </h2>
+                <div className='col-span-4'>
+                  <input type="password" 
+                    placeholder="Repeat the Password"
+                    onChange={(e) => setRepeatPassword(e.target.value)}
+                    value={repeatPassword}  
+                    className="input input-bordered w-full max-w-xs" 
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-span-12'></div>  
+
+          <div className='col-span-3 col-start-5'>
+            <button className='btn btn-error btn-circle btn-lg'
+              onClick={() => setStep(1)} >
+              <ArrowSmLeftIcon className='h-8 duration-300 hover:scale-125 transtion east-out' />
+            </button>
+          </div>
+          <div className='col-span-3 '>
+            <button className={`btn btn-accent btn-circle btn-lg ${(password && repeatPassword && password === repeatPassword) ? '' : 'btn-disabled'}`} 
+              onClick={() => {
+                alert("DONE. Need to plugin redux logic here")
+                router.push('/')
+              }} >
+              <CheckIcon className='h-8 duration-300 hover:scale-125 transtion east-out' />
+            </button>
+          </div> 
+        </div>
+      }
+    </main>
+  );
+}
+
+export default ImportWallet;
