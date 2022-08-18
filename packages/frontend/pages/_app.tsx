a9deccc1115a4051490e7e8fb3daff1a4344f17a
Song Zhou <zs@ziorix.com>
2022-08-18 22:09:28 +0800

    analytics and upgrade yarn

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -11,6 +11,7 @@ import { ThemeProvider } from 'next-themes';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../features/redux/store';
+import useAckee from 'use-ackee';
 
 // import { persistStore } from 'redux-persist';
 // import { PersistGate } from 'redux-persist/integration/react';
@@ -26,9 +27,18 @@ Router.events.on('routeChangeStart', progress.start);
 Router.events.on('routeChangeComplete', progress.finish);
 Router.events.on('routeChangeError', progress.finish);
 
-// let persistor = persistStore(store);
-
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  
+  useAckee('/', {
+    server: 'https://analytics.choko.app',
+    domainId: 'd29f288d-14fa-47fc-b6dd-f06f4f72f5e3'
+  }, {
+    // TODO: cookie consent form for user
+    detailed: true,
+    ignoreLocalhost: true,
+    ignoreOwnVisits: true
+  })
+
   return (
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
