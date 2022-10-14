9c15ff7bed0bda7e915b5bf0d094901116f28d7f
yours <lizhuyao666@gmail.com>
2022-10-14 20:22:17 +0700

    update request modal

--- packages/frontend/pages/request/sign-tx.tsx
+++ packages/frontend/pages/request/sign-tx.tsx
@@ -205,10 +205,10 @@ function SignTxHandler (): JSX.Element {
 
       <Modal closeModal={closeModal}
         isOpen={openPasswordModal} >
-        <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+        <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white from-gray-900 to-black p-6 text-left align-middle shadow-xl transition-all border border-[#00f6ff] '>
           <Dialog.Title
             as='h3'
-            className='text-lg font-medium leading-6 text-gray-900'
+            className='font-poppins text-lg font-medium leading-6 text-black w-72'
           >
             Unlock Wallet with Password
           </Dialog.Title>
