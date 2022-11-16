9ab4330bb14395edb45b794544444801528e19ae
rocalex <rocmaildev@gmail.com>
2022-11-16 21:27:08 +0800

    added the erc20 send tx

--- packages/frontend/components/modal/SendTokenModal.tsx
+++ packages/frontend/components/modal/SendTokenModal.tsx
@@ -28,6 +28,8 @@ import { SignTxDescriptor, SignTxRequest, SignTxRequestPayload } from '@choko-wa
 import { ethers } from 'ethers';
 import { xxHash } from '@choko-wallet/core/util';
 import Keyring from '@polkadot/keyring';
+import { encodeContractCall } from '@choko-wallet/abi';
+
 
 /**
  * Modal wrapper to send crypto to another account
@@ -36,6 +38,8 @@ import Keyring from '@polkadot/keyring';
 interface Props {
   balanceInfo: BalanceInfo;
 }
+
+const LinkTokenABI = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]';
 
 const SendTokenModal = ({ balanceInfo }: Props): JSX.Element => {
   const { theme } = useTheme();
@@ -101,11 +105,56 @@ const SendTokenModal = ({ balanceInfo }: Props): JSX.Element => {
             await account.init();
             account.lock();
 
+            /* 
+            ** sending eth data
+            */
+            // const tx = {
+            //   chainId: 5,
+            //   to: '0xE8DAC12f7A4b0a47e8e2Af2b96db6F54e2E2C9C3',
+            //   value: ethers.utils.parseEther('0.0001'),
+            // };
+
+            /* 
+            ** erc20 token balance checking data
+            */
+            const data1 = encodeContractCall(
+              'erc20', 
+              'balanceOf', 
+              ['0xAA1658296e2b770fB793eb8B36E856c8210A566F']
+            )
+
+            /* 
+            ** approve max balance erc20 token data
+            */
+            const data2 = encodeContractCall(
+              '', 
+              'approve', 
+              [
+                '0x11760b4950F9981770049B3cC0D83CC8BC133247',
+                 ethers.constants.MaxUint256
+              ],
+              LinkTokenABI
+            )
+
+            /* 
+            ** sending erc20 link token data through abi
+            */
+            const data = encodeContractCall(
+              '', 
+              'transfer', 
+              [
+                '0x11760b4950F9981770049B3cC0D83CC8BC133247',
+                '1000000000000000000'  // send 1 Link token
+              ],
+              LinkTokenABI
+            )
+            console.log("data: ", data);
+
             const tx = {
               chainId: 5,
-              to: '0xE8DAC12f7A4b0a47e8e2Af2b96db6F54e2E2C9C3',
-              value: ethers.utils.parseEther('0.0001'),
-            };
+              data: data,
+              to: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB' // goerli link token contract address
+            }
             console.log('2');
 
             const serializedTx = ethers.utils.serializeTransaction(tx);
